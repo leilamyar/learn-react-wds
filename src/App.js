866929242d74c8c1cd42d5d6f16698b9df8f2e0b
leilamyar26 <leilamyar.dev@gmail.com>
2020-10-05 14:38:10 +0200

    feat(*) : toggle todo + how many left

--- src/App.js
+++ src/App.js
@@ -22,6 +22,17 @@ function App() {
   // in this case, we want to save new todos -> use local storage for that.
   // save todo in LS of key '...'
 
+  function toggleTodo(id) {
+    // id is stored in Todo component -> we need to pass that fn to TodoList
+    const newTodos = [...todos];
+    // create a copy because you should NEVER directly modify state
+    // 1st create a copy before modifying it
+    // then use that copy to set the new state
+    const todo = newTodos.find(todo => todo.id === id)
+    todo.complete = !todo.complete;
+    setTodos(newTodos);
+  }
+
   function handleAddTodo(e){
     const name = todoNameRef.current.value;
     if (name === '') return;
@@ -40,11 +51,11 @@ function App() {
 
   return (
     <>
-      <TodoList todos={todos}/>
+      <TodoList todos={todos} toggleTodo={toggleTodo}/>
       <input ref={todoNameRef} type="text" />
       <button onClick={handleAddTodo}>Add todo</button>
       <button>Clear complete</button>
-      <div>0 left to do</div>
+      <div>{todos.filter((todo) => !todo.complete).length} left to do</div>
     </>
   );
 }
--- src/Todo.js
+++ src/Todo.js
@@ -1,10 +1,15 @@
 import React from 'react'
 
-export default function Todo({todo}) {
+export default function Todo({todo, toggleTodo}) {
+
+  function handleToddoClick() {
+    toggleTodo(todo.id)
+  }
+
   return (
     <div>
       <label>
-        <input type="checkbox" checked={todo.complete}/>
+        <input type="checkbox" checked={todo.complete} onChange={handleToddoClick}/>
         {todo.name}
       </label>
     </div>
