3bd21734339abade6b1d65ec3082508dfbc0b35a
leilamyar26 <leilamyar.dev@gmail.com>
2020-10-05 14:11:45 +0200

    feat(todoList, Todo) : onclick add + localstorage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef, useEffect } from 'react';
+import TodoList from './TodoList'
+import uuidv4 from 'uuid/dist/v4';
+
+const LOCAL_STORAGE_KEY = 'todoApp.todos';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const todoNameRef = useRef();
+
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+    // JSON parse because is a String -> convert it to an array
+    if(storedTodos) setTodos(storedTodos)
+  }, [])
+  // 1 UEff to save todos, the other to LOAD todos
+  // empty arry, so it's called only once and will never change.
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
+  }, [todos])
+  // anytime our todos array changes we want to run that fn
+  // in this case, we want to save new todos -> use local storage for that.
+  // save todo in LS of key '...'
+
+  function handleAddTodo(e){
+    const name = todoNameRef.current.value;
+    if (name === '') return;
+    setTodos(prevTodos => {
+      return [ 
+        ...prevTodos, 
+        {
+          id: uuidv4(),
+          name: name,
+          complete: false,
+        }
+      ];
+    })
+    todoNameRef.current.value = null;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <TodoList todos={todos}/>
+      <input ref={todoNameRef} type="text" />
+      <button onClick={handleAddTodo}>Add todo</button>
+      <button>Clear complete</button>
+      <div>0 left to do</div>
+    </>
   );
 }
 
